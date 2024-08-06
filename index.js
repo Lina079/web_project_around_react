c973565c78eb7de51ee8f6a680d9a7b16129866d
Lina079 <linacastror079@gmail.com>
2024-08-06 19:25:18 +0200

    agregue codigo JS para el icono de corazon

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ document.addEventListener('DOMContentLoaded', function() {
   const closeButton = document.getElementById('close');
   const popup = document.querySelector('.popup');
   const backdrop = document.querySelector('.popup-backdrop');
+  const nameInput = document.getElementById('name');
+  const aboutInput = document.getElementById('about');
+  const saveButton = document.getElementById('saveButton');
+  const heartIcon = document.querySelectorAll('.element__container-heart');
+
 
 
 
@@ -19,15 +24,31 @@ document.addEventListener('DOMContentLoaded', function() {
       document.body.style.overflow = 'auto';
   }
 
+  function checkForm() {
+    if (nameInput.value.trim() !== '' && aboutInput.value.trim() !== '') {
+      saveButton.disabled = false;
+      saveButton.classList.add('enabled');
+    } else {
+      saveButton.disabled = true;
+      saveButton.classList.remove('enabled');
+    }
+  }
+
+
 
   openButton.addEventListener('click', openPopup);
-
-
   closeButton.addEventListener('click', closePopup);
-
-
   backdrop.addEventListener('click', closePopup);
-});
+  nameInput.addEventListener('input', checkForm);
+  aboutInput.addEventListener('input', checkForm);
+
+    heartIcon.forEach(function(heartIcon) {
+    heartIcon.addEventListener('click', function() {
+      heartIcon.classList.toggle('active');
+    });
+  });
+
+  });
 
 
 
