ba620dce89ca473ca0c249b2f19b9832583abf5d
Lina079 <linacastror079@gmail.com>
2024-09-05 20:33:03 +0200

    template incluido - popup add elements incluido - like funcional

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,75 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const openButton = document.getElementById('open');
-  const closeButton = document.getElementById('close');
-  const popup = document.querySelector('.popup');
+  const openButton = document.getElementById('open-edit');
+  const closeButton = document.getElementById('close-edit');
+  const openAddButtonElements = document.querySelector("#addButton-elements");
+  const closeAddButtonElements = document.querySelector("#close-addbutton-elements");
+  const popup = document.querySelector("#popup-form");
   const backdrop = document.querySelector('.popup-backdrop');
+  const popupElements = document.querySelector("#popup-elements");
+  const backdropElements = document.querySelector(".popup-backdrop-elements");
   const nameInput = document.getElementById('name');
+  const titleInput = document.querySelector("#title");
   const aboutInput = document.getElementById('about');
+  const placeInput = document.querySelector("#place");
   const saveButton = document.getElementById('saveButton');
+  const saveButtonElements = document.querySelector("#saveButton-elements");
   const profileName = document.querySelector('.profile__name');
   const profileDescription = document.querySelector('.profile__description');
-  const heartIcon = document.querySelectorAll('.element__container-heart');
+  const heartIcon = document.querySelectorAll(".heart-icon");
+  const elementTemplate = document.querySelector("#element-template").content;
+  const elementArea = document.querySelector(".elements");
+
+  const initialElements = [
+    {
+      name: "Valle de Yosemite",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg"
+    },
+    {
+      name: "Lago Louise",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg"
+    },
+    {
+      name: "Montañas Calvas",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg"
+    },
+    {
+      name: "Latemar",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg"
+    },
+    {
+      name: "Parque Nacional de la Vanoise",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg"
+    },
+    {
+      name: "Lago di Braies",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg"
+    }
+  ];
 
 
+  function createElement(name, link){
+    const element = elementTemplate.querySelector(".element__container").cloneNode(true);
+    const elementImage = element.querySelector(".element__container-photo");
+    const elementInfo = element.querySelector(".element__container-name-place");
+
+    elementInfo.textContent = name;
+    elementImage.src = link;
+    elementImage.alt = name;
+
+    const heartIcon = element.querySelector(".heart-icon");
+    if (heartIcon){
+        heartIcon.addEventListener('click', function(){
+          console.log('Heart icon clicked');
+          heartIcon.classList.toggle('active');
+        })
+    }
+
+    elementArea.append(element);
+  }
+
+  initialElements.forEach (function (item){
+    createElement(item.name, item.link);
+  })
 
 
   function openPopup() {
@@ -19,11 +78,24 @@ document.addEventListener('DOMContentLoaded', function() {
       document.body.style.overflow = 'hidden';
   }
 
+  function openPopupElements(){
+    popupElements.classList.add("popup_opened");
+    backdropElements.classList.add("backcdrop_visible");
+    document.body.style.overflow = 'hidden';
+  }
+
+
 
   function closePopup() {
       popup.classList.remove('popup_opened');
       backdrop.classList.remove('backdrop_visible');
       document.body.style.overflow = 'auto';
+  }
+
+  function closePoupElements(){
+    popupElements.classList.remove('popup_opened');
+    backdropElements.classList.remove('backdropElements_visible');
+    document.body.style.overflow = 'auto';
   }
 
   function checkForm() {
@@ -47,20 +119,29 @@ document.addEventListener('DOMContentLoaded', function() {
     closePopup();
   }
 
+  function createNewElements(){
+    if (titleInput.value.trim() !== '' ||  placeInput.value.trim() !== '') {
+      saveButtonElements.disabled = false;
+      saveButtonElements.classList.add('enabled');
+    } else {
+      saveButtonElements.disabled = true;
+      saveButtonElements.classList.remove('enabled');
+    }
+  }
 
 
   openButton.addEventListener('click', openPopup);
+  openAddButtonElements.addEventListener('click', openPopupElements);
   closeButton.addEventListener('click', closePopup);
+  closeAddButtonElements.addEventListener('click', closePoupElements);
   backdrop.addEventListener('click', closePopup);
+  backdropElements.addEventListener('click', closePoupElements)
   nameInput.addEventListener('input', checkForm);
   aboutInput.addEventListener('input', checkForm);
+  titleInput.addEventListener('input', createNewElements);
+  placeInput.addEventListener('input',createNewElements );
   saveButton.addEventListener('click', saveChanges);
 
-    heartIcon.forEach(function(heartIcon) {
-    heartIcon.addEventListener('click', function() {
-      heartIcon.classList.toggle('active');
-    });
-  });
 
   });
 
