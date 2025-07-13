2d1b3291acfe635568f24cd04ffc221a2503066e
Lina079 <linacastror079@gmail.com>
2025-07-13 12:38:01 +0200

    adicionar api al proyecto

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,5 +1,4 @@
-import { useState} from 'react';
-import initialCards from '../../utils/cardsData';
+import { useState, useEffect, useContext} from 'react';
 import Card from '../Card/Card';
 import NewCard from './components/NewCard/NewCard';
 import Popup from '../Popup/Popup';
@@ -8,12 +7,23 @@ import EditProfile from '../EditProfile/EditProfile';
 import ImagePopup from './components/ImagePopup/ImagePopup';
 import EditAvatar from './components/EditAvatar/EditAvatar';
 import ConfirmDelete from './components/ConfirmDelete/ConfirmDelete';
+import api from '../../utils/api';
 
   export default function Main() {
-    const [cards, setCards] = useState(initialCards);
+    const [cards, setCards] = useState([]);
     const [popup, setPopup] = useState(null);
     const [selectedImage, setSelectedImage] = useState(null);
     const [cardToDelete, setCardToDelete] = useState(null);
+
+    useEffect(() =>{
+      api.getCardList()
+        .then((cardsData) => {
+          setCards(cardsData);
+        })
+        .catch((error) => {
+          console.error('Error al cargar las tarjetas desde la API:', error);
+        });
+    }, []);
 
     const newCardPopup = {
     title: 'Nuevo lugar',
@@ -46,9 +56,9 @@ import ConfirmDelete from './components/ConfirmDelete/ConfirmDelete';
       />
 
       <section className="elements">
-       {cards.map((card, index) => (
+       {cards.map((card) => (
         <Card
-        key={index}
+        key={card._id}
         card={card}
         onImageClick={setSelectedImage}
         onDelete={() => setCardToDelete(card)}
---
+++ src/utils/api.js
@@ -0,0 +1,72 @@
+class Api {
+  constructor(baseUrl, headers) {
+    this.baseUrl = baseUrl;
+    this.headers = headers;
+  }
+
+  _handleResponse(res) {
+    if (res.ok) return res.json();
+    return Promise.reject(`Error: ${res.status}`);
+  }
+
+  getUserInfo () {
+    return fetch(`${this.baseUrl}/users/me`, {
+      method: 'GET',
+      headers: this.headers,
+    }).then(this._handleResponse);
+  }
+
+  getCardList() {
+    return fetch(`${this.baseUrl}/cards`, {
+      method: 'GET',
+      headers: this.headers,
+    }).then(this._handleResponse);
+  }
+
+  setUserInfo({ name, about}) {
+    return fetch(`${this.baseUrl}/users/me`, {
+      method: 'PATCH',
+      headers: this.headers,
+      body: JSON.stringify({ name, about }),
+    }).then(this._handleResponse);
+  }
+
+  addCard({ name, link }) {
+    return fetch(`${this.baseUrl}/cards`, {
+      method: 'POST',
+      headers: this.headers,
+      body: JSON.stringify({name, link}),
+    }).then(this._handleResponse);
+  }
+
+  deleteCard(cardId) {
+    return fetch(`${this.baseUrl}/cards/${cardId}`, {
+      method: 'DELETE',
+      headers: this.headers,
+    }).then(this._handleResponse);
+  }
+
+  changeLikeCardStatus(cardId, isLiked) {
+    return fetch(`${this.baseUrl}/cards/${cardId}/likes`, {
+      method: isLiked ? 'PUT' : 'DELETE',
+      headers: this.headers,
+    }).then(this._handleResponse);
+  }
+
+  setUserAvatar({ avatar }) {
+    return fetch(`${this.baseUrl}/users/me/avatar`, {
+      method: 'PATCH',
+      headers: this.headers,
+      body: JSON.stringify({ avatar }),
+    }).then(this._handleResponse);
+  }
+}
+
+const api = new Api("https://around-api.es.tripleten-services.com/v1", {
+      authorization: "6f3b5cfb-f567-4462-befe-fb5c9c2b0d52",
+      "Content-Type": "application/json",
+    })
+
+
+export default api;
+
